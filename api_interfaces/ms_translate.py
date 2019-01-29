b3324fcefb99761afb856310aed943a7c472dac8
willstrach <willstrach@gmail.com>
2019-01-29 22:02:40 +0000

    Moved translator service handling into api_interfaces and implemented a translator_manager, allowing multiple transation providers to exist

---
+++ api_interfaces/ms_translate.py
@@ -0,0 +1,98 @@
+import requests
+import requests
+import requests
+import json
+import json
+import json
+import uuid
+
+class MicrosoftTranslate():
+    ''' A class to handle interactions with the MS Translator Text API
+
+    Args:
+        api_key (str): The API Key for your instance of MS 
+    
+    Raises:
+        HTTPError: If a HTTP request fails
+    '''
+
+
+    def __init__(self, api_key):
+        self._base_url = 'https://api.cognitive.microsofttranslator.com/'
+        self._api_key = api_key
+        self._headers = {
+            'Ocp-Apim-Subscription-Key': api_key,
+            'Content-type': 'application/json'
+        }
+        self._supported_languages = self._get_supported_languages()
+
+    def _get_supported_languages(self):
+        request_url = self._base_url + 'languages?api-version=3.0&scope=translation'
+        response = requests.get(request_url, headers=self._headers)
+        if not response.ok:
+            raise requests.exceptions.HTTPError(f'\n\nFailed to contact MS Translator Text API:\n\tResponse returned error:\n\t\t{response.text}')
+        returnDict = {}
+        for language in response.json()['translation'].keys():
+            returnDict[language] = response.json()['translation'][language]['name']
+        return returnDict
+
+    def get_supported_languages(self):
+        ''' A method to get the languages supported by the MS Translator Text API
+
+        Returns:
+            dict: A dictionary of supported languages in the form language_tag:name, language tags follow the BCP 47 standard
+        '''
+        return self._supported_languages
+
+    def translate(self, inputString, translate_from=None, translate_to=['en'], html=False):
+        ''' A method to translate an input string to one or more languages
+
+        Args:
+            inputString (str): A string containing the source text to be translated
+            translate_from (:obj:'str',: optional): A string denoting the language tag of the source language (a list of supported languages and their tags can be found here: https://docs.microsoft.com/en-gb/azure/cognitive-services/Translator/language-support).
+                Defaults to None, if none, MS Translate will attempt to automatically recognise the language
+            translate_to (list(str)): A list of strings dentoting target language tags (a list of supported languages and their tags can be found here: https://docs.microsoft.com/en-gb/azure/cognitive-services/Translator/language-support)
+            html (:obj:'bool', optional): Does the source text contain HTML? Defaults to False
+
+        Returns:
+            dict: A dictionary containing the output of the request in the format language_tag:text 
+        
+        Raises:
+            ValueError: If translate_from or translate_to are not valid
+            HTTPError: If the HTTP request fails
+        '''
+
+        # Is translate_from valid?
+        if translate_from is not None:
+            if not translate_from in list(self._supported_languages.keys()):
+                raise ValueError(f'"{translate_from}" is not a recognised language')
+
+        # Is translate_to valid?
+        for language in translate_to:
+            if not language in self._supported_languages.keys():
+                raise ValueError(f'"{language}" is not a recognised language')
+
+        identifier = str(uuid.uuid4())
+
+        request_url = self._base_url + f'translate?api-version=3.0&ClientTraceId={identifier}' 
+        
+        if translate_from:
+            request_url += f'&from={translate_from}'
+
+        for language in translate_to:
+            request_url += f'&to={language}'
+
+        request_url += '&textType=html' if html else '&textType=plain'
+
+        data = json.dumps([{'text':inputString}])
+
+        response = requests.post(request_url, headers=self._headers, data=data)
+
+        if not response.ok:
+            raise requests.exceptions.HTTPError(f'\n\nFailed to contact MS Translator Text API:\n\tResponse returned error:\n\t\t{response.text}')
+
+        returnDict = {'request_id':identifier}
+        for translation in response.json()[0]['translations']:
+            returnDict[translation['to']] = translation['text']
+        
+        return returnDict
---
+++ api_interfaces/translator_manager.py
@@ -0,0 +1,82 @@
+from api_interfaces.ms_translate import MicrosoftTranslate
+
+
+class TranslatorManager():
+    ''' A class for managing multiple translation services. This is a mostly aspirational feature, where in the future I may add more API providers
+    '''
+
+    def __init__(self):
+        self.translation_services = {}
+        self._translator = None
+        self.current_service = None
+        self.supported_services = {
+            'MS':'Microsoft Translator Text API'
+        }
+
+
+    def add_translation_service(self, name, service, token):
+        ''' Add a translation service. If no translation services currently exist, this will set up the service as the current translator.
+
+        Args:
+            name (str): The name of the translator, this name will appear in menus
+            service (str): The name of the service, supported services are contained within the "supported_services" attribute of this object
+            token (str): The API token used to authenticate requests
+
+        Raises:
+            ValueError: If an unrecognised service name is passed
+            ValueError: If the name passed already exists
+        '''
+        if service not in list(self.supported_services.keys()):
+            raise ValueError(f'{service} is not a recognised service provider. Valid services are contained within the "supported_services" attribute of this object')            
+
+        if name in len(self.translation_services.keys()):
+            raise ValueError(f'{name} already exists!')
+
+        self.translation_services[name] = {}
+        self.translation_services[name]['service'] = service
+        self.translation_services[name]['token'] = token
+
+        if len(list(self.translation_services.keys())) == 1:
+        
+
+    def change_translation_service(self, name):
+        ''' Change the translation service in use.
+
+        Args:
+            name (str): The name of the translator
+        
+        Raises:
+            KeyError: If the translator name does not exist
+        '''
+        if self.translation_services[name]['service'] == 'MS':
+            self._translator = MicrosoftTranslate(translation_services[name]['token'])
+            self.current_service = name
+        
+
+    def translate(self, inputString, translate_from=None, translate_to=['en'], html=False):
+        ''' A method to translate an input string to one or more languages. Calls the translate method of the "_translator" attribute
+
+        Args:
+            inputString (str): A string containing the source text to be translated
+            translate_from (:obj:'str',: optional): A string denoting the language tag of the source language (a list of supported languages and their tags can be found here: https://docs.microsoft.com/en-gb/azure/cognitive-services/Translator/language-support).
+                Defaults to None, if none, MS Translate will attempt to automatically recognise the language
+            translate_to (list(str)): A list of strings dentoting target language tags (a list of supported languages and their tags can be found here: https://docs.microsoft.com/en-gb/azure/cognitive-services/Translator/language-support)
+            html (:obj:'bool', optional): Does the source text contain HTML? Defaults to False
+
+        Returns:
+            dict: A dictionary containing the output of the request in the format language_tag:text 
+        
+        Raises:
+            ValueError: If translate_from or translate_to are not valid
+            HTTPError: If the HTTP request fails
+        '''
+        return self._translator.translate(inputString, translate_from, translate_to, html)
+
+    
+    def get_supported_languages(self):
+        ''' A method to get the languages supported by the current translation service. Calls the get_supported_languages method of the "_translator" attribute
+
+        Returns:
+            dict: A dictionary of supported languages in the form language_tag:name, language tags follow the BCP 47 standard
+        '''
+        return self._translator.get_supported_languages()
